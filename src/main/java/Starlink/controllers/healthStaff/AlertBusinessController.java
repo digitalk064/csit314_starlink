5c4dfd7076e47e9b631bae7b7a2934b875a85f10
digitalk064 <nguynkhanh.lv@gmail.com>
2021-05-18 15:35:32 +0700

    Alert messages contain more details. Contact tracing now shows target user's location history with black outline. Alerting businesses no longer ignore businesses where no other user has been in contact.

--- src/main/java/Starlink/controllers/healthStaff/AlertBusinessController.java
+++ src/main/java/Starlink/controllers/healthStaff/AlertBusinessController.java
@@ -5,13 +5,13 @@ import java.util.List;
 import Starlink.entities.Alert;
 
 public class AlertBusinessController {
-    public boolean generateAlert(List<Integer> businesses) throws Exception
+    public boolean generateAlert(List<Integer> businesses, List<String> times) throws Exception
     {
         try{
             for(int i = 0; i < businesses.size(); i++)
             {
                 Alert alert = new Alert();
-                String message = "Dear business owner,\nOur system has recently determined that a person infected with Covid-19 has been inside your location.";
+                String message = "Dear business owner,\nOur system has recently determined that a person infected with Covid-19 has last been inside your location on " + times.get(i) + ".";
                 message += "\nPlease take necessary measures.\nThank you.";
                 alert.generateAlert(businesses.get(i), message);
             }
--- src/main/java/Starlink/controllers/healthStaff/AlertPublicUserController.java
+++ src/main/java/Starlink/controllers/healthStaff/AlertPublicUserController.java
@@ -3,23 +3,42 @@ package Starlink.controllers.healthStaff;
 import java.util.List;
 
 import Starlink.entities.Alert;
+import Starlink.entities.LocHistory;
 
 public class AlertPublicUserController {
-    public boolean generateAlerts(List<Integer> users) throws Exception
+    public boolean generateContactAlerts(List<LocHistory> contactRecords) throws Exception
     {
         try{
-            for(int i = 0; i < users.size(); i++)
+            for(LocHistory contactRecord : contactRecords)
             {
+                int userid = contactRecord.getPublicUserID();
+                if(userid == -1) //-1 means the location record belongs to the target/infected user, so ignore it
+                    continue;
+                String location = contactRecord.getAddress();
+                String time = contactRecord.getCheckIn();
                 Alert alert = new Alert();
-                String message = "Dear user,\nOur system has recently determined that you were in contact with a person who was found to be infected.";/*at one of the following locations: ";
-                /*
-                for(String loc : locations)
-                    message += loc + ", ";
-                */
+                String message = 
+                String.format("Dear user,\nOur system has recently determined that you were in contact with at least one person who was found to be infected at the following location: %s on %s",
+                location, time);
                 message += "\nPlease take necessary measures.\nThank you.";
-                alert.generateAlert(users.get(i), message);
+                alert.generateAlert(userid, message);
             }
             return true;
+        }catch(Exception e)
+        {
+            throw e;
+        }
+    }
+
+    public boolean generateInfectedAlert(int userid) throws Exception
+    {
+        try{
+            Alert alert = new Alert();
+            String message = "Dear user,\nYou have been deemed infected after testing.";
+            message += "\nPlease take necessary measures.\nThank you.";
+            alert.generateAlert(userid, message);
+        
+        return true;
         }catch(Exception e)
         {
             throw e;
